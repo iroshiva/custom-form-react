059e335908241fd652f5df65e72582fc2928c44b
iroshiva <jean.weil@protonmail.com>
2023-02-16 09:58:28 +0100

    Add submit btn + react-pdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 import uuid from "react-uuid";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+  PDFDownloadLink,
+} from "@react-pdf/renderer";
 import "./App.css";
 
 const SectionRows = ({ sectionRows, removeSection, onChange }) => {
@@ -7,10 +16,10 @@ const SectionRows = ({ sectionRows, removeSection, onChange }) => {
     return (
       <div key={row.id} className="sectionContainer">
         <label htmlFor={row.id}>Titre de section</label>
-        <input 
-          id={row.id} 
-          name="title" 
-          type="text" 
+        <input
+          id={row.id}
+          name="title"
+          type="text"
           placeholder="Titre de section"
           value={row.title}
           onChange={(e) => onChange(e, index)}
@@ -23,7 +32,51 @@ const SectionRows = ({ sectionRows, removeSection, onChange }) => {
   });
 };
 
+// Create styles
+const styles = StyleSheet.create({
+  viewer: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    // flexGrow: 1,
+    title: {
+
+    }
+  },
+});
+
+const MyDocument = ({ sectionRows }) => {
+  const displaySection = sectionRows.map((section, index) => {
+    return (
+      <View key={section.id} style={styles.section}>
+        <Text style={styles.section.title}>{index + 1 + "." } {section.title}</Text>
+      </View>
+    );
+  });
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        {displaySection}
+        {/* <View style={styles.section}>
+          <Text>Section #1</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View> */}
+      </Page>
+    </Document>
+  );
+};
+
 function App() {
+  const [documentDisplay, setDocumentDisplay] = useState(false);
   const [sectionRows, setSectionRows] = useState([
     {
       id: uuid(),
@@ -32,11 +85,11 @@ function App() {
   ]);
 
   const onChange = (e, sectionIndex) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     const newSectionRows = [...sectionRows];
     newSectionRows[sectionIndex][name] = value;
     setSectionRows(newSectionRows);
-  }
+  };
 
   const addSection = () => {
     const additionalSection = {
@@ -53,12 +106,47 @@ function App() {
     setSectionRows(sectionRowsCopy);
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formStorage = localStorage.getItem("formData");
+    if (formStorage !== JSON.stringify(sectionRows)) {
+      localStorage.setItem("formData", JSON.stringify(sectionRows));
+    }
+    console.log(sectionRows);
+    setDocumentDisplay(true);
+  };
+
   return (
     <div className="App">
-      <SectionRows sectionRows={sectionRows} removeSection={removeSection} onChange={onChange}/>
-      <button className="btn" onClick={addSection}>
-        +
-      </button>
+      {documentDisplay ? (
+        <>
+          <PDFViewer style={styles.viewer}>
+            <MyDocument sectionRows={sectionRows} />
+          </PDFViewer>
+          {/* <PDFDownloadLink document={<MyDocument {...props} />} fileName={"FileName"}>
+            <button> Download </button>
+          </PDFDownloadLink> */}
+        </>
+      ) : (
+        <form id="form">
+          <SectionRows
+            sectionRows={sectionRows}
+            removeSection={removeSection}
+            onChange={onChange}
+          />
+          <button type="button" className="btn" onClick={addSection}>
+            +
+          </button>
+          <button
+            form="form"
+            type="submit"
+            className="btn"
+            onClick={(e) => onSubmit(e)}
+          >
+            Enregistrer
+          </button>
+        </form>
+      )}
     </div>
   );
 }
